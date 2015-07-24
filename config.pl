e500820ddbcde43f5df9807ff5906fce310e1e4b
dokechin <user@user-PC.(none)>
2015-07-24 22:50:32 +0900

    layouts adjustments

--- config.pl
+++ config.pl
@@ -1,9 +1,16 @@
 +{
 title=>"週刊YAPC::Asia",
-left=>{title=>"Perlでロ｜ストビ｜フ",icon=>"https://avatars.githubusercontent.com/u/1422834?v=3"},
+left=>{title=>"自動調理でロ｜ストビ｜フ",icon=>"https://avatars.githubusercontent.com/u/1422834?v=3"},
 middles=>
   [{title=>"Perlでドロ｜ンが飛ぶ日",icon=>"http://pbs.twimg.com/profile_images/419456734264954880/FYDksW9H_normal.jpeg"},
    {title=>"ISCONの勝ち方",icon=>"https://avatars.githubusercontent.com/u/46764?v=3"},
+   {title=>"Perlワンライナ｜便利です",icon=>"http://graph.facebook.com/929494707114841/picture"},
+   {title=>"未だにES5時代のJS書いてません",icon=>"https://avatars.githubusercontent.com/u/26476?v=3"},
+   {title=>"今フロントエンドで何が起こっている",icon=>"https://avatars.githubusercontent.com/u/250407?v=3"},
+   {title=>"若手エンジニア達の生存戦略",icon=>"http://pbs.twimg.com/profile_images/599568269507342336/jb_Rk7sX_normal.jpg"},
+   {title=>"人間は間違える生き物である",icon=>"https://avatars.githubusercontent.com/u/374550?v=3"},
+   {title=>"趣味から育てたWebサ｜ビスで生きる",icon=>"https://avatars.githubusercontent.com/u/1041857?v=3"},
+   {title=>"サンタクロ｜スを支えるIT技術",icon=>"http://graph.facebook.com/100001054610433/picture"},
   ],
 right=>{title=>"PHP帝国は滅んでいない\n逆襲の機会を虎視眈々とねらっている",icon=>"http://pbs.twimg.com/profile_images/378800000122330373/c9162e04e2a5fb779758cc4f0a13c2c0_normal.jpeg"},
 };
--- test.pl
+++ test.pl
@@ -12,7 +12,7 @@ $|=1;
 
 my $config = config_do 'config.pl';
 
-#get_icon_images($config);
+get_icon_images($config);
 make_cyuzuri($config);
 
 sub make_cyuzuri {
@@ -23,10 +23,19 @@ sub make_cyuzuri {
     my $white = Imager::Color->new("#ffffff");
     my $black = Imager::Color->new("#000000");
     my $font = Imager::Font->new( file => 'ipagp.ttf' ,color => $white, size=> 48);
-    my $font_s_b = Imager::Font->new( file => 'ipagp.ttf' ,color => $black, size=> 32);
-    my $font_l = Imager::Font->new( file => 'ipagp.ttf' ,color => $white, size=> 64);
+    my $font_s_b = Imager::Font->new( file => 'ipagp.ttf' ,color => $black, size=> 30);
+    my $font_l = Imager::Font->new( file => 'ipaexg.ttf' ,color => $white, size=> 64);
+    my $font_m = Imager::Font->new( file => 'ipaexm.ttf' ,color => $white, size=> 64);
 
     my $named_img = Imager->new(file => "nakazuri.png");
+
+    my $file = md5_hex($config->{left}->{icon}) . ".png";
+    my $img = Imager->new(file=> $file);
+    $named_img->paste(left=>64, top=>632, src=>$img);
+
+    $file = md5_hex($config->{right}->{icon}) . ".png";
+    $img = Imager->new(file=> $file);
+    $named_img->paste(left=>832, top=>632, src=>$img);
 
     # create textbox instance
     my $tb = Imager::DTP::Textbox::Vertical->new(
@@ -42,31 +51,24 @@ sub make_cyuzuri {
     my $index = 0;
     for my $middle(@{$config->{middles}}){
 
+         my $file = md5_hex($middle->{icon}) . ".png";
+         my $img = Imager->new(file=> $file);
+         $named_img->paste(left=>608- ($index * 48), top=> 540, src=>$img);
+
          $tb = Imager::DTP::Textbox::Vertical->new(
              text=> decode("utf-8", $middle->{title}),font=>$font_s_b, wrapWidth=>64, wrapHeight=>640);
                  
-         $tb->draw(target=>$named_img,x=>640 - ($index * 64) ,y=>10);
+         $tb->draw(target=>$named_img,x=>640 - ($index * 48) ,y=>10);
 
-         my $file = md5_hex($middle->{icon}) . ".png";
-         my $img = Imager->new(file=> $file);
-         $named_img->paste(left=>576- ($index * 64), top=>480, src=>$img);
 
          $index++;
 
     }
 
     $tb = Imager::DTP::Textbox::Horizontal->new(
-        text=> decode("utf-8", $config->{title}),font=>$font_l, wrapWidth=>500, wrapHeight=>64);
+        text=> decode("utf-8", $config->{title}),font=>$font_m, wrapWidth=>500, wrapHeight=>64);
             
     $tb->draw(target=>$named_img,x=>256,y=>640);
-
-    my $file = md5_hex($config->{left}->{icon}) . ".png";
-    my $img = Imager->new(file=> $file);
-    $named_img->paste(left=>64, top=>656, src=>$img);
-
-    $file = md5_hex($config->{right}->{icon}) . ".png";
-    $img = Imager->new(file=> $file);
-    $named_img->paste(left=>832, top=>656, src=>$img);
 
     $named_img->write(file=> $output);
 
@@ -89,16 +91,20 @@ sub get_icon_image{
     my $img = Imager->new();
     $img->read(data=> $res->content);
 
-    my $xpixcels = 64;
-    my $ypixcels = 64;
+    my $xpixcels = 48;
+    my $ypixcels = 48;
     if ($size == 2){
-        $xpixcels = 136;
-        $ypixcels = 136;
+        $xpixcels = 160;
+        $ypixcels = 160;
     }
 
     $img = $img->scale(xpixels=> $xpixcels, ypixels=> $ypixcels);
 
-    $img->write(file=>$file);
+    my $img_dst = $img->convert(
+       matrix => [ [ .4, .4, .2 ] ]
+    );
+
+    $img_dst->write(file=>$file);
 
 }
 
